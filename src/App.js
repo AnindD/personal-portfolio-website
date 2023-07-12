c9d5c3cf71e367880ec1c13316e28a4c437355ed
AnindD <anindit.dewan@gmail.com>
2023-07-12 13:42:19 -0400

    Added information to skills section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-//import React, { useState } from 'react'
+import React from 'react'
 
 function App() {
   let Component; 
@@ -42,24 +42,91 @@ function Navbar() {
 
 function Skills() {
   return ( 
-    <p>Here are a collection of my skills</p>
+    <React.Fragment>
+      <div>
+      <h2>Skills</h2>
+      <p>Here is a showcase of all my technical skills I have built through the development of my projects.</p>
+      </div>
+      <h2>Languages</h2>
+      <div>
+      <p>
+        <ul>
+          <li>4 years of experience in Python</li>
+          <li>3 years experience in HTML, CSS and JavaScript.</li>
+          <li>2 years experience in Java</li>
+          <li>Project experience working with SQL</li>
+          <li>Currently learning C++/C</li>
+        </ul>
+      </p>
+      </div>
+      <div>
+        <h2>Technical Experience</h2>
+        <p>
+          <ul>
+            <li>SQL commands and building basic databases (i.e. Account Systems)</li>
+            <li>Web Development</li>
+            <ul>
+              <li>Frontend - ReactJS</li>
+              <li>Backend - Django & ExpressJS</li>
+            </ul>
+            <li>Data Structures</li>
+            <ul>
+              <li>Arrays & Hashmaps</li>
+              <li>Stacks & Queues</li>
+              <li>Singly Linked Lists</li>
+            </ul>
+            <li>Algorithms</li>
+            <ul>
+              <li>Big O Analysis and Algorithmic Complexity</li>
+              <li>Linear and Binary Search</li>
+              <li>Insertion, Bubble, Quick, etc. Sorting Algorithms</li>
+            </ul>
+            <li>Git and Version Control Procedures</li>
+            <li>Object Oriented Programming & Markup Languages</li>
+            <li>Basic arduino commands</li>
+            <li>Low level understanding of computer networks (i.e. TCP/IP Protocol)</li>
+        </ul>
+        </p>
+      </div>
+      <div>
+        <h2>Languages</h2>
+        <p id="bottomP">
+          <ul>
+            <li>Native in oral and written English</li>
+            <li>Native in oral Chakma</li>
+            <li>Intermediate/working proficiency in oral Bangla</li>
+            <li>Intermediate/working proficiency in oral and written French</li>
+          </ul>
+        </p>
+      </div>
+
+    </React.Fragment>
   )
 }
 
 function Projects() { 
   return ( 
-    <div>
-      <h2>My Work</h2>
-      <p>Here is a showcase of all my projects from beginning to end. My interests are primarily in web development, machine learning and hardware electronics and how we can synthesize them together.</p>
-      <h2>Awards and Certifications</h2>
-      <p>
-        <ul>
-          <li>2nd place in The 2022 <a href="https://devpost.com/software/propel-r52yae">Queens University Summer Computing Challenge</a></li>
-          <li>Silver medalist in the 2023 TDSB skills competition <br/> within the coding subcategory</li>
-        </ul>
-      </p>
-      <h2>Projects</h2> 
-    </div>
+    <React.Fragment>
+      <div>
+        <h2>My Work</h2>
+        <p>Here is a showcase of all my projects from beginning to end. My interests are primarily in web development, machine learning and hardware electronics and how we can synthesize them together.</p>
+        <h2>Awards and Certifications</h2>
+        <p>
+          <ul>
+            <li>2nd place in The 2022 <a id="pLink" href="https://devpost.com/software/propel-r52yae">Queens University Summer Computing Challenge</a></li>
+            <li>Silver medalist in the 2023 TDSB skills competition <br/> within the coding subcategory</li>
+          </ul>
+        </p>
+        <h2>Projects</h2>
+      </div> 
+      <div className="card-container">
+        <img src="https://picsum.photos/300/200" alt="Card Image" className="card-img"/>
+        <h2 className="card-title">Card Title</h2>
+        <p className="card-description">This is a card description. You can add more details about the card </p>
+        <a href="cardPage" className="card-btn">Learn More</a>
+      </div>
+
+    </React.Fragment>
   )
 }
 
